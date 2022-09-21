bd98ea64e9293500137bb51a7cec3546605bc000
Fatmagül Demirbaş <dfatmagul72@gmail.com>
2022-09-21 22:47:50 +0300

    İkinci bir ekran oluşturuldu

---
+++ SegueApp2/SecondViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SecondViewController.swift
+//  SegueApp2
+//
+//  Created by Fatmagül Demirbaş on 21.09.2022.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+
+    @IBOutlet weak var SecondView: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    
+
+}
